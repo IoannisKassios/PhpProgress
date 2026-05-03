c08b7c904819933f7d565b079f8396df8b44e64d
Ioannis K. <ioannis.kassios@proton.me>
2026-05-03 13:59:18 +0300

    Add basic HTML form for weekly pay calculation

---
+++ index.php
@@ -0,0 +1,34 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="index.php" method="post">
+        <label>hours:</label>
+        <input type="text" name="hours">
+        <input type="submit" value="total">
+    </form>
+
+</body>
+</html>
+
+<?php
+
+    $hours = $_POST["hours"];
+    $rate = 15;
+    $weeklyPay = null;
+
+    if($hours == 0){
+        $weeklyPay = 0;
+    }
+    else{
+        $weeklyPay = $hours * $rate;
+    }
+
+    echo "You made \${$weeklyPay} this week!!";
+
+?>
